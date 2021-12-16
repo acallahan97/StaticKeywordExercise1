4f3edab0128eb8f0219f4e1904e21035cd76ad6e
acallahan97 <callahanamanda877@gmail.com>
2021-12-16 08:56:45 -0700

    initial commit

---
+++ StaticKeywordExercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticKeywordExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.CelsiusToFarenheit(33.1);
+            TempConverter.FarenheitToCelsius(98.2);
+        }
+    }
+}
---
+++ StaticKeywordExercise1/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise1
+{
+    class TempConverter
+    {
+        public static double FarenheitToCelsius(double f)
+        {
+            return f * -17.222222;
+        }
+        public static double CelsiusToFarenheit(double c)
+        {
+            return c * 33.8;
+        }
+    }
+}
